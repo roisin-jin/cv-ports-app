650a42c5c4b8ff50d2799e7695ac4145126d3662
roisin-jin <jin@ringkinces.com>
2015-04-15 19:04:40 +0100

    Minor update

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,34 @@
 package controllers
 
+import org.slf4j.{LoggerFactory, Logger}
 import play.api._
 import play.api.mvc._
+import play.modules.reactivemongo.MongoController
+import play.modules.reactivemongo.json.collection.JSONCollection
 
-object Application extends Controller {
+object Application extends Controller with MongoController {
+
+  private final val logger: Logger = LoggerFactory.getLogger(classOf[Application])
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    logger.info("Serving index page...")
+    Ok(views.html.index("Hello Roisin!"))
   }
+
+  /*
+   * Get a JSONCollection (a Collection implementation that is designed to work
+   * with JsObject, Reads and Writes.)
+   * Note that the `collection` is not a `val`, but a `def`. We do _not_ store
+   * the collection reference to avoid potential problems in development with
+   * Play hot-reloading.
+   */
+  def collection: JSONCollection = db.collection[JSONCollection]("ports")
+
+  // ------------------------------------------ //
+  // Using case classes + Json Writes and Reads //
+  // ------------------------------------------ //
+
+  import models._
+  import models.JsonFormats._
 
 }
---
+++ app/models/port.scala
@@ -0,0 +1,17 @@
+package models
+
+/**
+ * Created by roisin_jin on 14/04/2015.
+ */
+case class Polygon (lat: Double, lon: Double, ele: Double)
+
+case class Port (name: String, locode: String, polygon: Polygon)
+
+object JsonFormats {
+
+  import play.api.libs.json.Json
+
+  // Generates Writes and Reads for Feed and Port
+  implicit val polygonFormat = Json.format[Polygon]
+  implicit val portFormat = Json.format[Port]
+}
